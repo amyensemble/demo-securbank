992067bb2ef5c6718f9a3b3cd52e16edad486497
Amy Oulton <amyo@ensemble.com>
2026-03-24 15:09:32 -0700

    fix breaking code

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,18 +18,21 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: ['**/*.test.js', '**/*.spec.js', 'playwright.config.js'],
+    }],
     'xwalk/max-cells': ['error', {
-      '*': 4, // default limit
-      form: 15,
+      '*': 4, // default limit for all models
+      form: 17,
       wizard: 12,
       'form-button': 7,
       'checkbox-group': 20,
       checkbox: 19,
       'date-input': 21,
-      'drop-down': 19,
+      'drop-down': 20,
       email: 22,
       'file-input': 20,
-      'form-fragment': 15,
+      'form-fragment': 16,
       'form-image': 7,
       'multiline-input': 23,
       'number-input': 22,
@@ -43,8 +46,9 @@ module.exports = {
       modal: 11,
       rating: 18,
       password: 20,
-      tnc: 12
+      tnc: 12,
+      range: 19
     }],
-    'xwalk/no-orphan-collapsible-fields': 'off'
+    'xwalk/no-orphan-collapsible-fields': 'off', // Disable until enhancement is done for Forms properties
   },
 };
